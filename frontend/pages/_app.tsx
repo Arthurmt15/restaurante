3e90965379d7cfd2f9d76b708b8f20314cfff404
arthur.oliveira <@arthur.oliveira>
2026-07-15 08:39:32 -0300

    comentario: componente raiz da aplicacao

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
+// Componente raiz que envolve todas as páginas com Layout e estilos globais
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
